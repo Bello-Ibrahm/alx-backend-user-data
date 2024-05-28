7d921b6bc093c42143ce1b4453bb176783540eba
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-05-28 20:52:48 +0100

    Task 4. Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Authentication Module """
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    '''
+    '''
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
