4bd17bec2de35d70cadad4f99e0d94a7c6ce9614
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-05-31 21:20:27 +0100

    Update task 17 - Route to POST request

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,16 +73,13 @@ def profile() -> str:
     abort(403)
 
 
-@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
-def update_password() -> str:
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def reset_pwd() -> str:
     '''
     '''
     email = request.form.get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
-
     try:
-        AUTH.update_password(reset_token, new_password)
+        reset_token = AUTH.get_reset_password_token(email)
     except ValueError:
         abort(403)
 
